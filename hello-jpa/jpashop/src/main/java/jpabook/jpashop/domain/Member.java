97fb42031b6ac70a15e8db321970d2045d914a9f
eunhee <dmsgml9903@gmail.com>
2021-09-18 14:21:05 +0900

    RDS에 맞춘 mapping

---
+++ hello-jpa/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,43 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Member {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column(name = "USERNAME")
+    private String username;
+
+    @Column(name = "TEAM_ID") // 왜래키를 일대일로 DB와 맞춤
+    private Long teamId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Long getTeamId() {
+        return teamId;
+    }
+
+    public void setTeamId(Long teamId) {
+        this.teamId = teamId;
+    }
+}
