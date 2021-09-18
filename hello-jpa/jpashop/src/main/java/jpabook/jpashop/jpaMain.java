7f7f23cd3356985c95e0f322a4ef080116117706
eunhee <dmsgml9903@gmail.com>
2021-09-18 14:16:58 +0900

    RDS에 맞춘 테이블 매핑

--- hello-jpa/jpashop/src/main/java/jpabook/jpashop/jpaMain.java
+++ hello-jpa/jpashop/src/main/java/jpabook/jpashop/jpaMain.java
@@ -1,5 +1,8 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Team;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -13,6 +16,21 @@ public class jpaMain {
         tx.begin();
 
         try {
+            // 등록
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setTeamId(team.getId()); // 객체 지향스럽지 않다.
+            em.persist(member);
+
+            // 조회
+            Member findMember = em.find(Member.class, member.getId());
+            Long findTeamId = findMember.getTeamId();
+            Team findTeam = em.find(Team.class, findTeamId); // 객체 지향스럽지 않다.
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback(); // 롤백
