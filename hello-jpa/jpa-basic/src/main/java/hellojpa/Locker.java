35100f2c181ac8f04edcdadb7dea913824a2a062
eunhee <dmsgml9903@gmail.com>
2021-10-08 11:30:50 +0900

    1:1 mapping :대상테이블에 있는 외래키 관리

--- hello-jpa/jpa-basic/src/main/java/hellojpa/Locker.java
+++ hello-jpa/jpa-basic/src/main/java/hellojpa/Locker.java
@@ -16,4 +16,10 @@ public class Locker {
     // * 일대일 -> 양방향, 읽기전용
     @OneToOne(mappedBy = "locker")
     private Member member;
+
+    /* 대상 테이블에 외래 키 단방향 정리
+    *  1. 단방향 관계는 jpa지원X
+    *  2. 양방향 관계는 지원 */
+
+    /* 내가 내 것만 관리!! */
 }
