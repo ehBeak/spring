8b7fa6c8b0cb0b4136ec6aa99b9ad0f3b28014b7
eunhee <dmsgml9903@gmail.com>
2021-10-13 18:57:28 +0900

    @MappedSuperClass

---
+++ hello-jpa/jpa-basic/src/main/java/hellojpa/BaseEntity.java
@@ -0,0 +1,47 @@
+package hellojpa;
+
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+
+/* 공통된 속성 상속 시키기 위한 클래스(그냥 단순 공통) */
+@MappedSuperclass // 매핑 정보만 받는 superClass
+public abstract class BaseEntity {
+
+    private String createBy;
+    private LocalDateTime createdDate;
+    private String lastModifiedBy;
+    private LocalDateTime lastModifiedDate;
+
+    public String getCreateBy() {
+        return createBy;
+    }
+
+    public void setCreateBy(String createBy) {
+        this.createBy = createBy;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getLastModifiedBy() {
+        return lastModifiedBy;
+    }
+
+    public void setLastModifiedBy(String lastModifiedBy) {
+        this.lastModifiedBy = lastModifiedBy;
+    }
+
+    public LocalDateTime getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(LocalDateTime lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+}
--- hello-jpa/jpa-basic/src/main/java/hellojpa/jpaMain.java
+++ hello-jpa/jpa-basic/src/main/java/hellojpa/jpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class jpaMain {
@@ -18,21 +19,16 @@ public class jpaMain {
          tx.begin();
 
          try {
-             // movie 등록 : item테이블에 insert 쿼리, movie테이블에 insert쿼리가 각각 나간다.
-             Movie movie = new Movie();
-             movie.setDirector("aaaa");
-             movie.setActor("bbbb");
-             movie.setName("바람과 함께 사라지다");
-             movie.setPrice(10000);
+             // SuperClass 상속받은 MemberClass -> create 쿼리는 1개
 
-             em.persist(movie);
+             Member member = new Member();
+             member.setCreateBy("aaa");
+             member.setCreatedDate(LocalDateTime.now());
+             em.persist(member);
 
              em.flush();
              em.close();
 
-             // Item으로 객체를 찾을시, union all로 모든 테이블을 다 뒤진다. : 복잡한 쿼리 생성
-             Item item = em.find(Movie.class,movie.getId());
-             System.out.println("findMovie = " + item);
 
 
 
