e11d1ef9d05892948b91caaf8e00456e17b5bece
eunhee <dmsgml9903@gmail.com>
2021-12-06 16:20:27 +0900

    jpql : 기본 문법과 쿼리 API

--- hello-jsql/src/main/java/jpql/JpaMain.java
+++ hello-jsql/src/main/java/jpql/JpaMain.java
@@ -1,9 +1,7 @@
 package jpql;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.*;
+import java.util.List;
 
 public class JpaMain {
 
@@ -16,10 +14,49 @@ public class JpaMain {
 
         try {
 
+            Member member3 = new Member();
+            member3.setUsername("member1");
+            member3.setAge(10);
+            em.persist(member3);
+
             Member member = new Member();
-            member.setUsername("member1");
+            member.setUsername("member2");
+            member.setAge(10);
             em.persist(member);
 
+            // TypeQuery : 반환 타입이 명확할 때 사용
+            TypedQuery<Member> query = em.createQuery("select m from Member m", Member.class);
+            TypedQuery<String> query1 = em.createQuery("select m.username from Member m", String.class);
+
+            // Query : 반환 타입이 명확하지 않을 때 사용
+            Query query2 = em.createQuery("select m.username, m.age from Member m");
+
+            // 결과 조회 API
+
+            // 1. 값이 여러개일때
+            List<Member> resultList = query.getResultList();
+
+            for(Member member1 : resultList) {
+                System.out.println(member1);
+            }
+
+            // 2. 값이 무조건 하나일때(진짜 결과가 하나일 때만 사용해야함)
+            /*Member result2 = query.getSingleResult();
+            System.out.println(result2);*/
+
+            // 2-1 예외)값이 하나 이상일 때 : 리스트 반환, javax.persistence.NonUniqueResultException
+            // 2-2 예외)값이 아예 없을 때 : 빈리스트 반환, javax.persistence.NoResultException
+
+            // 파라미터 바인딩
+            Member result = em.createQuery("select m from Member m where m.username = :username", Member.class)
+                    .setParameter("username", "member1")
+                    .getSingleResult();
+
+            System.out.println("result = " + result.getUsername());
+
+            /*TypedQuery<Member> query3 = em.createQuery("select m from Member m where m.username = :username", Member.class);
+            query3.setParameter("username", "member1");
+            System.out.println(query3.getSingleResult().getUsername());*/
             em.flush();
 
             tx.commit();
