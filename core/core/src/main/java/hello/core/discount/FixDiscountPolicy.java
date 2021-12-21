fa3f2caa0134619c2b5d193944a83405c9923a01
eunhee <dmsgml9903@gmail.com>
2021-12-21 15:28:51 +0900

    회원도메인~주문할인 도메인 개발

---
+++ core/core/src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -0,0 +1,17 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class FixDiscountPolicy implements DiscountPolicy{
+
+    private int discountFicAmount = 1000;
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return discountFicAmount;
+        }
+        return 0;
+    }
+}
