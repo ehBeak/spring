6fe807b015904577fd2385e1c765aab121fadbac
eunhee <dmsgml9903@gmail.com>
2022-01-04 15:45:53 +0900

    애노테이션 직접 만들기

--- core/core/src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ core/core/src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -1,13 +1,16 @@
 package hello.core.discount;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.member.*;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
-@Component
+
 //@Qualifier("mainDiscountPolicy")
-@Primary
+//@Primary
+@Component
+@MainDiscountPolicy
 public class RateDiscountPolicy implements DiscountPolicy{
 
     private int discountPercent = 10;
