1a264b33952653cd7a3f1e0bf66f0b3e670d0724
eunhee <dmsgml9903@gmail.com>
2022-02-14 15:35:04 +0900

    간단한 주문 조회 V2: 엔티티를 DTO로 변환

--- jpashop/jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ jpashop/jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,15 +1,21 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /*
  * xToOne
@@ -32,4 +38,37 @@ public class OrderSimpleApiController {
         }
         return all;
     }
+
+    @GetMapping("/api/v2/simple-orders")
+    public Result orderV2() {
+        List<Order> all = orderRepository.findAllByCriteria(new OrderSearch());
+        List<SimpleOrderDto> collect = all.stream()
+                .map(order -> new SimpleOrderDto(order))
+                .collect(Collectors.toList());
+        return new Result(collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        T data;
+    }
+
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+        }
+    }
+
 }
