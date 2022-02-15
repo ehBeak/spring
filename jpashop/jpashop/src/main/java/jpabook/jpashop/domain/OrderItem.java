447393b1a71758fa77f488f6e8263bb0c295e6f3
eunhee <dmsgml9903@gmail.com>
2022-02-15 10:41:04 +0900

    주문조회 V1,V2: 엔티티 직접 노출, 엔티티를 DTO로 변환

--- jpashop/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ jpashop/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jpabook.jpashop.domain.item.Item;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -19,10 +20,12 @@ public class OrderItem {
     @Column(name = "order_item_id")
     private Long id;
 
+
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "item_id")
     private Item item;
 
+    @JsonIgnore
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "order_id")
     private Order order;
