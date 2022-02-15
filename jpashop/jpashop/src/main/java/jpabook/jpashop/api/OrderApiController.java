b896784479a3e9b9bcee9e80ea7249909a11edeb
eunhee <dmsgml9903@gmail.com>
2022-02-15 14:03:22 +0900

    주문조회 V3.1: 엔티티를 DTO로 변환 - 페이징과 한계 돌파

--- jpashop/jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ jpashop/jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -9,6 +9,7 @@ import jpabook.jpashop.repository.OrderSearch;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -45,6 +46,18 @@ public class OrderApiController {
     @GetMapping("/api/v3/orders")
     public List<OrderDto> orderV3() {
         List<Order> all = orderRepository.findAllWithItem();
+        List<OrderDto> collect = all.stream()
+                .map(order -> new OrderDto(order))
+                .collect(Collectors.toList());
+        return collect;
+    }
+
+    @GetMapping("/api/v3.1/orders")
+    public List<OrderDto> orderV4(
+            @RequestParam(value = "offset", defaultValue = "0") int offset,
+            @RequestParam(value = "limit", defaultValue = "100") int limit) {
+        List<Order> all = orderRepository.findAllWithMemberDelivery(offset, limit);
+
         List<OrderDto> collect = all.stream()
                 .map(order -> new OrderDto(order))
                 .collect(Collectors.toList());
