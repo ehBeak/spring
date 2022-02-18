4e9e2d08290f7c9fab1e3bc1d48542a82cfda087
eunhee <dmsgml9903@gmail.com>
2022-02-18 23:38:20 +0900

    ~영속성 관리 - 내부 동작 방식

---
+++ hello-jpa/jpa-basic/src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,41 @@
+package hellojpa;
+
+import hellojpa.domain.Member;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            //== Transaction start==//
+            Member member = new Member();
+            member.setId(1L);
+            member.setName("HelloA");
+            em.persist(member);
+
+            Member findMember = em.find(Member.class, 1L);
+
+            findMember.setName("HelloJPA");
+
+            em.remove(findMember);
+
+            //== Transaction end ==//
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
