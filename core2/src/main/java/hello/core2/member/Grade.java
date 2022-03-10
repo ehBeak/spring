c0de6a29b9301298d2d905f29bdfcbb9a207c02a
eunhee <dmsgml9903@gmail.com>
2022-03-10 09:07:40 +0900

    스프링 핵심 원리 이해1 - 예제 만들기

---
+++ core2/src/main/java/hello/core2/member/Grade.java
@@ -0,0 +1,5 @@
+package hello.core2.member;
+
+public enum Grade {
+    VIP
+}
---
+++ core2/src/main/java/hello/core2/order/OrderService.java
@@ -0,0 +1,9 @@
+package hello.core2.order;
+
+import hello.core2.member.Member;
+
+public interface OrderService {
+
+    Order createOrder(Long memberId, String itemName, int itemPrice);
+
+}
