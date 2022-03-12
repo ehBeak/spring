7e0046b93ca0c8cb45f4b26d3687df856a5ec3be
eunhee <dmsgml9903@gmail.com>
2022-03-12 18:12:00 +0900

    의존관계 자동 주입

--- core2/src/main/java/hello/core2/discount/FixDiscountPolicy.java
+++ core2/src/main/java/hello/core2/discount/FixDiscountPolicy.java
@@ -1,8 +1,14 @@
 package hello.core2.discount;
 
+import hello.core2.annotation.MainDiscountPolicy;
 import hello.core2.member.Grade;
 import hello.core2.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
+@Component
+//@Qualifier("fixDiscountPolicy")
+@MainDiscountPolicy
 public class FixDiscountPolicy implements DiscountPolicy{
 
     private int discountFixAmount = 1000;
--- core2/src/main/java/hello/core2/order/OrderServiceImpl.java
+++ core2/src/main/java/hello/core2/order/OrderServiceImpl.java
@@ -1,21 +1,22 @@
 package hello.core2.order;
 
+import hello.core2.annotation.MainDiscountPolicy;
 import hello.core2.member.Member;
 import hello.core2.member.MemberRepository;
 import hello.core2.discount.DiscountPolicy;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+//@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
-//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
     private final DiscountPolicy discountPolicy;
 
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
