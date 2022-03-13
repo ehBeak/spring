13b0879a383a0dfabd52b93bf7e2dda90653075c
eunhee <dmsgml9903@gmail.com>
2022-03-13 09:05:43 +0900

    생명주기 콜백

---
+++ core2/src/test/java/hello/core2/lifecycle/NetworkClient.java
@@ -0,0 +1,60 @@
+package hello.core2.lifecycle;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class NetworkClient /*implements InitializingBean, DisposableBean*/ {
+
+    private String url;
+
+    public NetworkClient() {
+        System.out.println("생성자 호출, url = " + url);
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void connect() {
+        System.out.println("url = " + url);
+    }
+    
+    public void call(String message) {
+        System.out.println("call: " + url);
+        System.out.println("message = " + message);
+    }
+
+    public void disconnect() {
+        System.out.println("close: " + url);
+    }
+
+
+/*    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }*/
+
+    @PostConstruct
+    public void init(){
+        System.out.println("NetworkClient.init");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @PreDestroy
+    public void close() {
+        System.out.println("NetworkClient.close");
+        disconnect();
+    }
+}
